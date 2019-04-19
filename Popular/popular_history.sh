75a7d524dd48c7cd6fcb4875a5300d2e01e3d387
Will Hutwagner <36306338+whutwagner3@users.noreply.github.com>
2019-04-19 10:06:44 -0400

    Update popular_history.sh

--- Popular/popular_history.sh
+++ Popular/popular_history.sh
@@ -1,5 +1,6 @@
 #! usr/bin/env bash
 
 python3 popular.py ../ShannonBoyko/ShannonBoyko_All.txt > keepthese..txt
-../plink --file ../ShannonBoyko/ShannonBoyko_All --keep keepthese.txt --out ./ThoseKept --noweb --dog
+../plink --file ../ShannonBoyko/ShannonBoyko_All --keep keepthese.txt --out ./ThoseKept --make-bed --dog
+../plink --bfile ThoseKept
 
