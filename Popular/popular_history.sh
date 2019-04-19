93963f28825d9f2606dda28b1bc9cbfe3e8e70a4
Will Hutwagner <36306338+whutwagner3@users.noreply.github.com>
2019-04-19 10:23:04 -0400

    Update popular_history.sh

--- Popular/popular_history.sh
+++ Popular/popular_history.sh
@@ -1,6 +1,6 @@
 #! usr/bin/env bash
 
-python3 popular.py ../ShannonBoyko/ShannonBoyko_All.txt > keepthese.txt
+python3 popular.py ../ShannonBoyko/ShannonBoyko_All.txt ./ShannonBoyko_All.poptxt> keepthese.txt
 ../plink --file ../ShannonBoyko/ShannonBoyko_All --keep keepthese.txt --out ./ThoseKept --make-bed --dog
 ../plink2 --bfile ThoseKept --pca 2 --out ThoseKept --dog
-
+python3 ../interact.py ShannonBoyko_All.poptxt ThoseKept.eigenvec output.html
